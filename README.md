b8317cfc61c4d7f9e9f68b3d6603e7156890f5d6
Nicardo Lannaman <nicardolannaman@gmail.com>
2024-06-16 15:45:18 -0500

    Simplification and Finalization of Dark and Light Modes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RobotChatBot
+
+This is intended to train a bot to respond to you. This is a Flask app which will require you to have python installed. 
--- static/script.js
+++ static/script.js
@@ -1,9 +1,28 @@
 const toggles = document.querySelectorAll('.material-symbols-outlined');
+const body = document.querySelector('body');
+const chatContainer = document.querySelector('.chat-container');
+const personalizeInput = document.querySelector('input');
+
 
 toggles.forEach(element => {
     element.addEventListener('click', btnClicked);
 });
 
 function btnClicked() {
-    alert("Btns Clicked");
+    body.classList.toggle('night-mode');
+    chatContainer.classList.toggle('night-mode');
+    personalizeInput.classList.toggle('night-mode');
+    // alert("Btns Clicked");
+
+    if (body.classList.contains('night-mode')) {
+        toggles.forEach(element => {
+            element.innerHTML =`light_mode`;
+    
+    });
+    } else {
+        toggles.forEach(toggle => {
+            toggle.innerHTML = `mode_night`;
+        });
+    }
+
 }
